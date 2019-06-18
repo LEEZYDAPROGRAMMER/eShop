af719ca22cd9a742899d657071e9774678e62673
leezydaprogrammer <lesley.t.ncube@gmail.com>
2019-06-18 15:19:18 +0200

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IfStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -10,38 +11,66 @@ export class AppComponent {
   cartItems=[];
   numCart:number=0;
  totalPrice: any=0;
-   tempPrice: any=0;
+ qty: number=0; 
+
+   
   items=[{itemName:"NMD",picUrl:"https://m.media-amazon.com/images/I/71JxsngqQOL._AC_UL320_.jpg",desc:"Addidas NMD sneaker",price:2500},{itemName:"YEEZY",picUrl:"https://m.media-amazon.com/images/I/71-IPjT029L._AC_UL320_.jpg",desc:"Adidas YEEZY sneaker",price:4000},{itemName:"Jordans",picUrl:"https://m.media-amazon.com/images/I/71PM3pW+mmL._AC_UL320_.jpg",desc:"Nike Jordan sneaker",price:3000}];
   
   addToCart(item)
   {
     
     let index= this.items.indexOf(item);
-   
-  this.cartItems.push({itemName:this.items[index].itemName,picUrl:this.items[index].picUrl,desc:this.items[index].desc,price:this.items[index].price/*,qty:0*/});
-    this.numCart+=1;
+
  
- /*
-    // qty  
-    this.cartItems[index].qty++
-    */
+    
+    // if(this.cartItems.length == 0)
+    // {
+      
+    //   this.cartItems.push({itemName:this.items[index].itemName,picUrl:this.items[index].picUrl,desc:this.items[index].desc,price:this.items[index].price,qty:this.qty+1})
+    //   this.doesNot = true;
+    // }
+    // else{
+      
+   
+    
+    
+      var product= this.cartItems.find(x =>x.itemName===item.itemName);
+
+      if(product)
+        {
+         
+          this.cartItems[index].qty =  this.cartItems[index].qty + 1
+         
+        }
+     else
+        {
+          
+          this.cartItems.push({itemName:this.items[index].itemName,picUrl:this.items[index].picUrl,desc:this.items[index].desc,price:this.items[index].price,qty:this.qty+1})
+         
+        }
+
+
+    
+
+ //   }
+   
     //total
-    this.tempPrice=this.cartItems[index].price;
-    this.totalPrice +=this.tempPrice; 
+    
+    this.totalPrice +=item.price; 
     
   }
   delete(cartI)
   {
-    let index= this.items.indexOf(cartI);
+    let index= this.cartItems.indexOf(cartI);
     this.cartItems.splice(index,1);
    this.numCart-=1;
    //total
-   this.tempPrice=cartI.price;
-   this.totalPrice -= this.tempPrice;
- /*   // qty
-     this.cartItems[index].qty--;
-     */
-    
+  
+   this.totalPrice=this.totalPrice-(cartI.price*cartI.qty);
+   
+  // qty
+   cartI.qty--;
+     
   }
  checkout(cartI)
   {
